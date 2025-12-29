f83c165db19acfbbb656e2a1d15464ce20d6c403
abhikumar0607 <abhikumar06071998@gmail.com>
2025-12-29 17:12:07 +0530

    dd

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,7 +4,6 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Gate;
-use Illuminate\Support\Facades\Broadcast;
 use App\Models\User;
 
 class AppServiceProvider extends ServiceProvider
@@ -22,12 +21,12 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        Gate::define('is-admin', fn ($user) => $user->role === 'admin');
-        Gate::define('is-customer', fn ($user) => $user->role === 'customer');
+        Gate::define('is-admin', function ($user) {
+        return $user->role === 'admin';
+        });
 
-        Broadcast::routes(['middleware' => ['web', 'auth']]);
-
-        require base_path('routes/channels.php');
+        Gate::define('is-customer', function ($user) {
+            return $user->role === 'customer';
+        });
     }
-
 }
