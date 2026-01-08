5c7f12be8f132c0c91e82518644943228cf29fe3
abhikumar0607 <abhikumar06071998@gmail.com>
2026-01-08 17:35:50 +0530

    ddd

---
+++ resources/views/components/connection-tab.blade.php
@@ -0,0 +1,31 @@
+<div class="container text-center main-color-11">
+    <ul class="nav nav-pills justify-content-left">
+        <li class="nav-item">
+            <a class="nav-link1 {{ request()->routeIs('customer.match.suggestions') ? 'active' : '' }}"
+                href="{{ route('customer.match.suggestions') }}">
+                Match Suggestions
+            </a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link1 {{ request()->routeIs('customer.my.connections') ? 'active' : '' }}"
+                href="{{ route('customer.my.connections') }}">
+                My Connections
+            </a>
+        </li>
+         <span class="badge bg-danger">{{ $myConnectionscount ?? 0 }}</span>
+        <li class="nav-item"> 
+            <a class="nav-link1 {{ request()->routeIs('customer.sent.requests') ? 'active' : '' }}"
+                href="{{ route('customer.sent.requests') }}">
+                Sent Requests
+            </a>
+        </li>
+        <span class="badge bg-danger">{{ $sentRequestsCount ?? 0 }}</span>
+        <li class="nav-item">
+            <a class="nav-link1 {{ request()->routeIs('customer.received.requests') ? 'active' : '' }}"
+                href="{{ route('customer.received.requests') }}">
+                Received Requests
+            </a>
+        </li>
+         <span class="badge bg-danger">{{ $receivedRequestsCount ?? 0 }}</span>
+    </ul>
+</div>
--- resources/views/customer/connection/received-requests.blade.php
+++ resources/views/customer/connection/received-requests.blade.php
@@ -25,34 +25,7 @@
       </div>
    </section>
    <section class="pb-3">
-      <div class="container text-center main-color-11">
-         <ul class="nav nav-pills justify-content-left">
-            <li class="nav-item">
-               <a class="nav-link1 {{ request()->routeIs('customer.match.suggestions') ? 'active' : '' }}"
-                  href="{{ route('customer.match.suggestions') }}">
-               Match Suggestions
-               </a>
-            </li>
-            <li class="nav-item">
-               <a class="nav-link1 {{ request()->routeIs('customer.my.connections') ? 'active' : '' }}"
-                  href="{{ route('customer.my.connections') }}">
-               My Connections
-               </a>
-            </li>
-            <li class="nav-item"> 
-               <a class="nav-link1 {{ request()->routeIs('customer.sent.requests') ? 'active' : '' }}"
-                  href="{{ route('customer.sent.requests') }}">
-               Sent Requests
-               </a>
-            </li>
-            <li class="nav-item">
-               <a class="nav-link1 {{ request()->routeIs('customer.received.requests') ? 'active' : '' }}"
-                  href="{{ route('customer.received.requests') }}">
-               Received Requests
-               </a>
-            </li>
-         </ul>
-      </div>
+        <x-connection-tab />
    </section>
    <section class="pb-5">
       <div class="container">
