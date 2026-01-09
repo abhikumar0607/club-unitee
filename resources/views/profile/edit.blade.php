c5a12fe7629c66e46315d9650f4d3c0675a60315
abhikumar0607 <abhikumar06071998@gmail.com>
2026-01-09 14:59:39 +0530

    ddd

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -43,11 +43,13 @@
                 </div>
             </div>
 
+            @can('is-customer')
             <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
                 <div class="max-w-xl">
                     @include('profile.partials.delete-user-form')
                 </div>
             </div>
+            @endcan
         </div>
     </div>
 </div>
